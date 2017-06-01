375552fb4c7100309e1f5b14c40f80a9d8e77121
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-01 00:54:05 +0000

    made Examples/Plot.hs work with recent version of diagrams

--- Examples/Plot.hs
+++ Examples/Plot.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, BangPatterns, ScopedTypeVariables, TupleSections, 
              RecordWildCards, NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import AI.SVM.Simple
@@ -7,7 +8,7 @@ import qualified Data.Vector.Storable as V
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.Backend.Cairo
-
+import Diagrams.Backend.Cairo.Internal (Options(CairoOptions))
 
 
 main = do
@@ -18,22 +19,23 @@ main = do
                        ,('i', V.fromList [0.5,0.5::Double])
                         ]
     let (m,svm2) = trainClassifier (C 1) (RBF 4) trainingData
-    let plot = 
-               (circle # fc green # scale 5 5 )
+    let plot :: QDiagram Cairo V2 Double Any
+        plot = circle 1 # fc green # scale 5
                `atop` 
-               (circle # fc green # scale 5 5
-               `atop` circle # scale 100 100 # lineWidth 5) # translate (200,200) 
+               (circle 1 # fc green # scale 5 `atop` circle 1 # scale 100 # lineWidth 5) # translate (V2 200 200)
                `atop` 
-               (circle # fc green # scale 5 5 # translate (400,400) )
+               (circle 1 # fc green # scale 5 # translate (V2 400 400) )
                `atop` 
-               foldl (atop) (circle # scale 1 1)
-               [circle # scale 5 5 # translate (400*x,400*y) # fc (color svm2 (x,y))
+               foldl (atop) (circle 1 # scale 1)
+               [circle 1 # scale 5 # translate  (V2 (400*x) (400*y)) # fc (color svm2 (x,y))
                | x <- [0,0.025..1], y <- [0,0.025..1]] 
-    fst $ renderDia Cairo (CairoOptions ("test.png") (PNG (400,400))) plot
+    fst $ renderDia Cairo (CairoOptions ("test.png") (dims2D 400 400) PNG False ) plot
   where
     color svm (x,y) = case classify svm  [x,y] of
                         'r' -> red
                         'b' -> blue
                         'i' -> indigo
- 
+
+
 between a x b = a <= x && x <= b
+
